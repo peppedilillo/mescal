d1f84e136198dbe1d3c0ac2877a4efe3d8f3d0cd
ezmarche <105488703+ezmarche@users.noreply.github.com>
2022-05-13 11:35:42 +0200

    Add files via upload

---
+++ calib_stretcher.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Campana, R.; Della Casa, G.; Marchesini, E. J.
+"""
+
+import numpy as np
+import lmfit
+import re
+import seaborn as sns
+from lmfit.models import GaussianModel, LinearModel, PolynomialModel, Model
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from scipy.signal import find_peaks
+from scipy.signal import find_peaks_cwt
+from scipy.signal import argrelextrema
+from scipy.signal import savgol_filter
+import os
+from itertools import islice
+import statistics
+from specutilities import *
+from pathlib import Path
+import warnings
+
+"""
+To run this script, a file named stretcher_X_*.txt, * being A, B, C or D (quadrant) should be provided
+which should include:
+1. a line with the number of channels to be analysed, including those that were turned off (i.e., 0, 1, 2, ..., 31)
+2. a line with the channels that were turned off
+3. a line with the threshold below which all data are discarded (pedestal) for each channel, including those turned off
+"""
+
+# visualize=True to visualize every intermediate step through several plots
+visualize = True
+
+# smooth=True to apply an smoothing filter to the raw data in order to easily find the peaks
+# Note that every other operation, including calibration, is always done with the *non* smoothed data
+smooth = True
+
+
+# ranges[1,2] defines the lower and upper limits of the spectra, in ADC channels
+# while step defines how many channels will be contained inside one single bin
+ranges = [1000,2400]
+step = 2
+
+calibration = True
+cleangamma = False
+asics = ['C']
+
+fitsfiles = ['QC_FeAm.fits']
+datafile = 'calib_lines.txt'
+
+
+for i in range(len(asics)):
+    ASIC = asics[i]
+    
+
+    Path("./Quad" + ASIC).mkdir(parents=True, exist_ok=True)
+
+    #Files should be named in an uniform way
+    #so that the filename depends only on the ASIC
+    #fitstiles= [['DatafileX_'+ASIC]]
+    
+    fitsfile = fitsfiles[i]
+
+    # Reading the data from the stretcher_X_*.txt file and slicing the inputs into variables
+    with open("stretcher_X_%s.txt" % ASIC, 'r') as f:
+        N_channel = np.loadtxt(islice(f, 1), delimiter=" ", dtype= int)
+    
+        Off_CHst = np.loadtxt(islice(f, 1), delimiter=" ", dtype= int)
+        CHst = np.setdiff1d(range(0, np.size(N_channel)), Off_CHst, assume_unique=False)
+
+        pedestal = np.loadtxt(islice(f, 1), delimiter=" ", dtype= float)
+
+        couples = np.loadtxt(f, delimiter=" ", dtype= int)
+
+
+
+    asic_fit_results = [None]*32
+
+
+    print("Reading file", fitsfile, "using", datafile, "\n")
+    
+    # Preparing the variables needed to work
+    outputfile, counts_data, line_data, calib_units = dataprep(datafile, fitsfile, ASIC)
+
+
+    # Cleaning the data by selecting gamma events
+    if cleangamma:
+
+        print(couples)  
+        counts_data = clean_gamma(counts_data, couples, threshold=1100)          
+        
+    
+    n_peaks = len(line_data)
+    
+    
+    
+
+    fwhm=np.empty([len(N_channel),n_peaks,2])
+    xadc=np.empty([len(N_channel),n_peaks,2])
+
+
+    # Fit the defined peaks for the spectrum, one channel at a time
+    for v in np.arange(32):    
+        if v in CHst: # If the channel is in the list
+            window = 5*step         
+            w_size = 9              
+    
+            print('REDUCING ASIC {:s} CH {:02d} STRETCHER\n'.format(ASIC, v))
+        
+            channel_data = counts_data.field("CH_{:02d}".format(v))
+
+            # building the raw spectrum
+            x, y = hist(pedestal[v], ranges, step, channel_data, 'stretcher')
+
+            plt.figure()
+            plt.plot(x,y,drawstyle='steps-mid',label='Original data')
+            plt.title("CH {:02d}".format(v))
+
+            try:
+                # smoothing (if required), and find the position of each peak
+                limit, peak, indexlist = smoothing(x, y, w_size, n_peaks, window, smooth)
+                for l in limit:
+                    plt.axvspan(l[0], l[1], alpha=0.2, color='green')
+                # Fitting peaks for the given spectrum
+                fit_results = fitPeaks(x, y, limit, visualize=visualize)
+                if asic_fit_results[v] is not None:
+                    asic_fit_results[v] = np.row_stack((asic_fit_results[v], fit_results))
+                else:
+                    asic_fit_results[v] = fit_results
+            except:
+                fit_results = np.column_stack(([0.]*n_peaks, [0.]*n_peaks, [0.]*n_peaks, [0.]*n_peaks))
+                asic_fit_results[v] = fit_results
+            
+            if visualize:
+                plt.show()    
+            
+            # Add line in summary file
+            output_summary_string = "{0:s}\t{1:02d}\t".format(ASIC, v)
+
+
+            for p in range(len(fit_results)):
+                output_summary_string += "{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}\t".format(fit_results[p,0],fit_results[p,2],fit_results[p,1],fit_results[p,3])
+
+
+                fwhm[v,p,0]=fit_results[p,2]
+                fwhm[v,p,1]=fit_results[p,3]
+
+                xadc[v,p,0]=fit_results[p,0]
+                xadc[v,p,1]=fit_results[p,1]
+
+
+            outputfile.write(output_summary_string+"\n")
+        else:
+            print ('Skipping CH {:02d} ASIC {:s} STRETCHER\n'.format(v, ASIC)) 
+    outputfile.close()
+
+
+
+    if calibration:
+        
+        filecal = open('./Quad'+ASIC+'/Calibration_Stretcher_ASIC_{:s}.txt'.format(ASIC), "w")
+        filecal.write("# CH\tGain\tGain_err\tOffset\tOffset_err\tChi2\n")
+        
+        gainv=np.empty([len(N_channel),2])
+        offsetv=np.empty([len(N_channel),2])
+        ev_calib=np.empty([len(channel_data),len(N_channel)])
+
+        
+        for v in np.arange(32):
+            if v in CHst: # If the channel is in the list
+                # Now calibrate using all the found lines
+    
+                print('CALIBRATING ASIC {:s} CH {:02d} STRETCHER\n'.format(ASIC, v))   
+                channel_data = counts_data.field("CH_{:02d}".format(v))     
+
+                chi2, gain, gain_err, offset, offset_err, xcalib_line = calibrate(ASIC,v,line_data, asic_fit_results[v], verbose=False)
+
+                channel_data_calib=(channel_data-offset)/gain
+                
+                ev_calib[:,v]=channel_data_calib
+
+                print("Calibration for ASIC {:s} CH {:02d}".format(ASIC, v))
+                print("Gain: {0:.3f} +- {1:.3f}\nOffset: {2:.3f} +. {3:.3f}\n".format(gain, gain_err, offset, offset_err))
+
+                gainv[v,0]=gain
+                gainv[v,1]=gain_err
+                offsetv[v,0]=offset
+                offsetv[v,1]=offset_err
+                
+
+                with sns.plotting_context("talk"):
+
+
+                    fig1, ax = linPlot('stretcher',ranges,v,xadc[v],xcalib_line,gain,offset,calib_units,figsize=(16,6))
+                    fig1.suptitle('QUADRANT '+ASIC)
+                    fig1.savefig('./Quad'+ASIC+'/'+ASIC+'_LINEARITY_CH'+str(v)+'.png')
+                    plt.close(fig1)
+
+
+                cal_summary_string = "{:02d}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\n".format(v, gain, gain_err, offset, offset_err, chi2)
+                filecal.write(cal_summary_string)
+
+                pedestalcal=(pedestal[v]-offsetv[v,0])/gainv[v,0]
+                rangescal=(ranges-offsetv[v,0])/gainv[v,0]
+                asiccal=(channel_data-offsetv[v,0])/gainv[v,0]
+                stepcal=step/gainv[v,0]
+                
+                windowcal=5*stepcal
+
+                xcalib,ycalib=hist(pedestalcal,rangescal,stepcal,asiccal,'stretcher')
+
+                with sns.plotting_context("talk"):
+                    fig3=plt.figure(figsize=(16,6))
+                    plt.suptitle('QUADRANT '+ASIC)
+        
+                    specPlot(xcalib,ycalib,rangescal,v,n_peaks,xcalib_line,windowcal,'stretcher')
+    
+                    plt.savefig('./Quad'+ASIC+'/'+ASIC+'_XSPEC_CH_'+str(v)+'.png')
+                    plt.close(fig3)
+    
+                #hdulistfe = fits.open('QA_Fe55.fits')
+                #counts_data_fe = hdulistfe[1].data
+                #hdulistfe.close()
+    #
+                #channel_data_fe = counts_data_fe.field("CH_{:02d}".format(v))     
+#
+                #asiccal_fe=(channel_data_fe-offsetv[v,0])/gainv[v,0]
+                #xcalibraw_fe,ycalibraw_fe=hist(0,rangescal,stepcal,asiccal_fe,'stretcher')
+                #xlines_fe=([5.89,6.49])
+                #with sns.plotting_context("talk"):
+                #    fig4=plt.figure(figsize=(16,6))
+                #    plt.suptitle('QUADRANT '+ASIC)
+        #
+                #    specPlot(xcalibraw_fe,ycalibraw_fe,rangescal,v,2,xlines_fe,windowcal,'stretcher')
+    #
+                #    plt.savefig('./Quad'+ASIC+'/'+ASIC+'_XSPEC_CH_'+str(v)+'_Fe55_raw.png')
+                #    plt.close(fig4)
+    
+
+        filecal.close()
+
+        specx=ev_calib[ev_calib >= 2.0]
+
+
+        table_hdu = fits.BinTableHDU.from_columns([fits.Column(name='X_evt',format='D',array=specx)])
+        table_hdu.writeto('./Quad'+ASIC+'/Spectrum_X_'+ASIC+'.fits',overwrite=True)
+
+
+    with sns.plotting_context("paper"):
+
+        fig2=plt.figure(figsize=(16,6))
+        plt.suptitle('QUADRANT '+ASIC)
+        
+        gmean,omean=gainPlot(ASIC,N_channel,'stretcher',gainv,offsetv,fwhm,CHst,Off_CHst)
+
+        plt.savefig('./Quad'+ASIC+'/'+ASIC+'_GAIN_OFFSET.png')
+        plt.close(fig2)
+    
---
+++ specutilities.py
@@ -0,0 +1,505 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Campana, R.; Della Casa, G.; Marchesini, E. J.
+"""
+
+import numpy as np
+import lmfit
+import re
+import matplotlib.patches as mpatches
+from lmfit.models import GaussianModel, LinearModel, PolynomialModel, Model
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from scipy.signal import find_peaks
+from scipy.signal import find_peaks_cwt
+from scipy.signal import argrelextrema
+from scipy.signal import savgol_filter
+import os
+import statistics
+
+
+def specPlot(x,y,ranges,ch,npeak,lines,window,mode):
+
+    if mode=='shaper' or mode=='Shaper':
+        labels='Shaper'
+        colormain='blue'
+        colorlines='yellow'
+    if mode=='stretcher' or mode=='Stretcher':
+        labels='Stretcher'
+        colormain='red'
+        colorlines='green'
+
+
+
+    plt.title('CALIBRATION CH_'+str(ch))
+    plt.xlim(ranges[0],ranges[1])
+    plt.xlabel('Energy [keV]')
+    plt.ylabel('N')
+    plt.plot(x,y,color=colormain,linewidth=0.8,label=labels)
+
+    for i in range(0,npeak):
+
+        plt.axvspan(lines[i]-window,lines[i]+window,linestyle="dashed",color=colorlines,label=str(lines[i]),linewidth=0.6,alpha=0.5)
+    plt.axvspan(59.0,60.0,linestyle="dashed",color="purple",label="59.5 keV",linewidth=0.6,alpha=0.5)
+    plt.axvspan(57.70,57.72,linestyle="dashed",color="brown",label="59.5 keV -3%",linewidth=0.6,alpha=0.5)
+
+    plt.legend(loc="upper right",fontsize="x-small",frameon=False)
+
+
+
+def gainPlot(asic,ch,mode,gain,offset,fwhm,onchannels,offchannels):
+
+    if mode=='shaper' or mode=='Shaper':
+        labels='Shaper'
+        mark='s'
+        colormain='blue'
+        colorsec='royalblue'
+        colorthird='mediumturquoise'
+    
+    elif mode=='stretcher' or mode=='Stretcher':
+        labels='Stretcher'
+        mark='o'
+        colormain='red'
+        colorsec='indianred'
+        colorthird='lightpink'
+    
+    else:
+        labels=''
+        mark='x'
+        colormain='green'
+        colorsec='mediumseagreen'
+        colorthird='palegreen'
+
+
+
+    for i in (offchannels):
+        gain[i,:]=None 
+        offset[i,:]=None 
+        fwhm[i,:,:]=None 
+
+
+    K,N,L = np.shape(fwhm)
+
+
+    fmean=np.empty([N])
+    fmedian=np.empty([N])
+    gmean=np.empty([2])
+    omean=np.empty([2])
+
+    for i in range(0,N):
+        fmean[i]=np.nanmean(fwhm[:,i,0])
+
+    gmean[0]=statistics.mean(gain[onchannels,0])
+    gmean[1]=statistics.mean(gain[onchannels,1])
+    omean[0]=statistics.mean(offset[onchannels,0])    
+    omean[1]=statistics.mean(offset[onchannels,1])
+
+    plt.subplots_adjust(wspace=0.3,hspace=0.5)
+
+    if N==2 or N==3:
+        plt.subplot(2,N,1)
+    elif N==4 or N==5:
+        plt.subplot(2,N,2)
+    else:
+        plt.subplot(2,N,3)
+
+    plt.xlim(-0.5,31)
+
+    plt.title('GAIN vs. CH')
+    
+    plt.errorbar(ch, gain[:,0], yerr=gain[:,1], color=colormain, fmt=mark, label=labels,alpha=0.3)
+    plt.xticks(np.arange(0, 32, step=4)) 
+    
+    plt.fill_between(np.arange(-2,34),gmean[0]-(gmean[0]/5.),gmean[0]+(gmean[0]/5.),color=colorthird,alpha=0.5,label='Mean '+labels+' +/- 20%')
+    handles,labels=plt.gca().get_legend_handles_labels()
+    plt.legend(labels,loc='upper right',framealpha=1,fontsize='x-small')
+    
+    plt.xlabel('BE CH')
+    plt.ylabel('GAIN')
+    
+    
+    if N==2 or N==3:
+        plt.subplot(2,N,2)
+    elif N==4 or N==5:
+        plt.subplot(2,N,4)
+    else:
+        plt.subplot(2,N,6)
+    
+    plt.xlim(-0.5,31)
+    plt.title('OFFSET vs. CH')
+    plt.ylabel('OFFSET [instrum. units]')
+
+    plt.errorbar(ch, offset[:,0], yerr=offset[:,1], color=colormain, fmt=mark, alpha=0.3)
+    plt.xticks(np.arange(0, 32, step=3))  # Set label locations.
+    
+    
+    plt.axhline(omean[0],color=colorsec)
+    
+
+    plt.tick_params(axis='y', which='both', right=True,left=False,bottom=False,top=False,labelleft=False, labelright=True)
+    plt.xlabel('BE CH')
+
+    for i in range(0,N):
+        plt.subplot(2,N,N+1+i)
+        
+        plt.xlim(-0.5,31)
+
+        plt.ylim(np.nanmin(fwhm[:,i,0])-3*(np.nanmax(abs(fwhm[:,i,1]))),np.nanmax(fwhm[:,i,0])+3*(np.nanmax(abs(fwhm[:,i,1]))))
+
+        #plt.ylim(min(np.logical_not(np.isnan(fwhm[:,i])))-window,max(np.logical_not(np.isnan(fwhm[:,i])))+window)
+        plt.ylabel('FWHM [instrum. units]')
+
+        plt.title('Fit #'+str(i+1))
+        
+        plt.errorbar(ch, fwhm[0:32,i,0], yerr=fwhm[0:32,i,1],color=colormain, fmt=mark,alpha=0.3)
+        plt.xticks(np.arange(0, 32, step=3))  # Set label locations.
+        
+        plt.axhline(fmean[i],color=colorsec)
+        plt.tick_params(axis='y', which='both', right=True,left=False,bottom=False,top=False,labelleft=False, labelright=True)
+        plt.xlabel('BE CH')
+
+    return gmean,omean
+    
+
+
+def linPlot(mode,ranges,ch,xadc,xlines,gain,offset,calib_units, **kwargs):
+    """
+    Produces a plot with all the relevant information (centroid, fitting function, calibration function)
+    """
+    if mode=='shaper' or mode=='Shaper':
+        labels='Shaper'
+        mark='s'
+        colormain='blue'
+        colorsec='royalblue'
+        colorthird='mediumturquoise'
+    
+    elif mode=='stretcher' or mode=='Stretcher':
+        labels='Stretcher'
+        mark='o'
+        colormain='red'
+        colorsec='indianred'
+        colorthird='lightpink'
+    
+    else:
+        labels=''
+        mark='x'
+        colormain='green'
+        colorsec='mediumseagreen'
+        colorthird='palegreen' 
+    
+    fig, ax = plt.subplots(**kwargs)
+    ax.set_xlim(ranges[0],ranges[1])
+    ax.set_title('LINEARITY CH '+str(ch))
+    ax.set_ylabel('Amplitude ['+calib_units+']')
+    ax.set_xlabel('Centroid [instrum. units]')
+    
+    props = dict(boxstyle='round', facecolor='silver', alpha=0.5)
+    txt_string = "Gain: {:.3f}\nOffset: {:.1f}".format(gain, offset)
+    ax.text(0.05, 0.95, txt_string, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', bbox=props)
+    
+    ax.errorbar(xadc[:,0], xlines, xerr=xadc[:,1], fmt=mark, color=colorsec,markersize=8,alpha=0.5,label=labels)
+    ax.plot(float(gain)*np.array(xlines)+float(offset), xlines, lw=2, color=colormain,label='Best fit')
+    ax.legend(loc="upper right",fontsize="x-small",frameon=False)
+    #plt.show()
+    return fig, ax
+
+
+def fano(E):
+    """
+    Returns Fano noise in eV FWHM
+    Input: energy in keV
+    """
+    return np.sqrt(E*1000./3.6*0.118)*2.35*3.6
+
+
+def lineFitter(x, y, limits, bkg=None, verbose=True):  
+    """
+    Fits a Gaussian line between start and stop
+    Input:  x, y: input arrays
+            limits = (start,stop): tuple with fit boundaries
+            bkg (optional): polynomial background. If not None, should be polynomial degree (up to 7th deg.)
+    Output: lmfit result object
+    """
+    start = limits[0]
+    stop = limits[1]
+    y_err = np.sqrt(y)
+    x_start = np.where(x > start)[0][0]
+    x_stop  = np.where(x < stop)[0][-1]
+    x_fit = x[x_start:x_stop]
+    y_fit = y[x_start:x_stop]
+    y_fit_err = y_err[x_start:x_stop]
+
+    if bkg is not None:
+        mod = GaussianModel() + PolynomialModel(bkg, prefix='bkg_')
+    else:
+        mod = GaussianModel()
+    pars = mod.guess(y_fit , x=x_fit)
+    center = x_fit[np.argmax(y_fit)]
+    pars['center'].set(center, min=start, max=stop)
+    result = mod.fit(y_fit, pars, x=x_fit, weights=y_fit_err)
+   
+
+    x_fine = np.linspace(x[0],x[-1],len(x)*100)
+    fitting_curve = mod.eval(x=x_fine, \
+                         amplitude    =  result.best_values['amplitude'], \
+                         center       =  result.best_values['center'], \
+                         sigma        =  result.best_values['sigma'] )
+        
+    return result,start,stop,x_fine,fitting_curve
+
+
+def dataprep(datafile, fitsfile, ASIC):
+    """
+    Read the file with the expected lines and energies to be fitted the spectrum and prepare both input and output files.
+    Also it creates all the arrays that will be used for the analysis.
+
+    Input: datafile : .txt file of two columns with the name of the line and the peak energy
+           fitsfile : .fits file with the data for the spectrum
+           ASIC : quadrant in use (A, B, C, or D)
+       
+    Output: outputfile : .txt file to be filled for every ASIC, the header is created in the function
+            counts_data : data collected for the spectra from the FITS file
+            line_data = {line_name, line_details} : dictionary, respectively the name and an array with (energy, color) for every peak  
+    """
+    color = {0: "orange", 1: "cornflowerblue", 2: "indianred", 3: "green", 4: "yellow", 5:"purple", 6:"brown", 7:"black"}
+    # TODO: what happens if you have more lines than colors?
+    
+    line_data = {}
+    
+    infile = open(datafile)
+    for i,line in enumerate(infile):
+        lname, lenergy = line.split()
+        line_data[lname] = (float(lenergy), color[i])
+
+    hdulist = fits.open(fitsfile)
+    counts_data = hdulist[1].data
+    hdulist.close()
+    
+    outputfile = open('./Quad'+ASIC+'/Quad{:s}_{:s}'.format(ASIC, datafile),'w+')
+    string = "#ASIC  CH  "
+    for key in sorted(line_data):
+        string = string + "x_adc({0:s})  FWHM({0:s})   x_adc_err({0:s})  FWHM_err({0:s})".format(key)
+    outputfile.write(string)
+
+    
+    calib_units = ''.join(re.findall(r'[a-zA-Z]', lname))
+
+    return outputfile, counts_data, line_data, calib_units
+
+
+def smoothing(x, y, w_size, n_peak, window, smooth):
+    """
+    Applies a smoothing to the data collected and finds the peaks in the spectra.
+    
+    Inputs: x, y : arrays of data
+            w_size : integer to define the smoothing
+            n_peak : number of peaks to be found
+            window : integer defining the range around the peak
+            smooth : boolean, whether or not do the smoothing
+    Outputs: limit : the limits around the peak
+             peak : array of the peaks found
+    """
+    if smooth:
+        y_smooth = savgol_filter(y, w_size, 3) # window size 51, polynomial order 3
+        #plt.figure()
+        #plt.plot(x,y_smooth,drawstyle='steps-mid',label='smooth data')
+        #plt.title("CH {:02d}".format(v))
+        #plt.show()
+        peak, _ = find_peaks(y_smooth, height=1, width = 1, distance=20,prominence=25)
+        ytmp = y_smooth.copy()
+    else:
+        peak, _ = find_peaks(y,height=4, width = 5, distance=20,prominence=1)
+        ytmp = y.copy()
+        
+    index = np.zeros(n_peak)
+    
+         
+    # TODO: generalizzare la ricerca dei picchi a N
+
+    for i in range(n_peak):
+        index[i] = np.argmax(ytmp[peak])
+        ytmp[peak[int(index[i])]] = 0
+    
+    indexlist=sorted([int(x) for x in index])
+    
+    limit = np.zeros((n_peak,2))
+
+    for i in range(n_peak):
+        limit[i] = (x[peak[indexlist[i]]]-window,x[peak[indexlist[i]]]+window)
+    
+    return limit, peak, indexlist
+    
+
+
+def clean_gamma(counts_data, couples, threshold=1200):
+    """
+    Removes the data due to gammas, i.e. that are seen by both the SDD cells optically coupled with the same crystal, in order to do a proper calibration.
+
+    Input: counts_data : array containing the value measured by every channel for every event.
+           couples : array with the coupled channels.
+           threshold : integer defining when a channel has revealed a photon.
+    
+    Output: clean_data : same array of counts_data, without the gamma events.
+    """
+    print('Cleaning gamma-ray events with threshold', threshold)
+    print('Counts data original length', len(counts_data))
+        
+    for couple in couples:
+        print("Looking for events in pair:", couple)
+        pair_a, pair_b = couple
+        idx = np.where(np.logical_and( counts_data.field("CH_{:02d}".format(pair_a)) >= threshold, counts_data.field("CH_{:02d}".format(pair_b)) >= threshold))
+        clean_data = np.delete(counts_data, idx)
+    print('Counts data new length', len(clean_data))
+    print('Reduced of {:.2f}%'.format(len(clean_data)/len(counts_data)))
+    
+    return clean_data
+    
+
+def guess_pedestal(bins, counts, **kwargs):
+    """
+    a scipy.signal.find_peaks wrapper, 
+    it checks for peaks and returns the average
+    of the first two it finds (if more than two exist)
+    else returns None. by default find_peaks is operated
+    with parameters height = 10 and distance = 20. 
+    you can override these parameters or provide different ones 
+    e.g., guess_pedestal(bins,counts, height = 20, width = 20) will
+    call find_peaks(counts, height = 20, distance = 20, width = 20)
+    
+    params:
+    :param bins: sequence, histogram bins
+    :param counts: sequence, histogram counts
+    :param kwargs: other keyword arguments are passed to find_peaks.
+    :return: a number or None, pedestal value
+    """
+    default = {'height': 10, 'distance': 20}
+    params = {k:(kwargs[k] if k in kwargs else default[k]) 
+                for k in set(default)|set(kwargs)}
+    peaks, *_ = find_peaks(counts, **params)
+ 
+    if len(peaks) > 1:
+        return sum(bins[peaks[:2]])/2
+    return None
+
+
+def hist(pedestal, ranges, step, data, mode):
+    """
+    Prepare the data, binning it and removing the pedestal. 
+
+    Input: pedestal :  array given through a .txt file
+           ranges, step : array and int given in the main file 
+           data : array with the data to be prepared
+           mode : string, shaper or stretcher
+    
+    Output: x, y : array of data binned and with pedestal removed
+    """
+    binning = np.arange(ranges[0], ranges[1] + step, step)
+    x = binning[:-1] + step/2.
+    y, bins = np.histogram(data, bins=binning)
+
+    pedestal2=guess_pedestal(x, y)
+    #print(pedestal,pedestal2)
+
+    if mode == "stretcher":
+        pedestal_limit = np.where(x >= pedestal)[0][0]
+        y[0:pedestal_limit] = 0
+            
+    return x, y
+
+
+def fitPeaks(x, y, limits, visualize=False):
+    """
+    Fit the peaks in the given spectrum, each in the given limits
+    
+    Input: 
+            x, y : arrays with input spectrum
+            limits : array of limits for each peak 
+    Output:
+            fit_results : array, rows is each peak, column is mu, mu_err, fwhm, fwhm_err
+    """
+    n_peaks = len(limits)
+    x_adc = np.zeros(n_peaks)
+    x_adc_err = np.zeros(n_peaks)
+    sigma = np.zeros(n_peaks)
+    sigma_err = np.zeros(n_peaks)
+    
+    for i in range(n_peaks):
+        result, start, stop ,x_fine, fitting_curve = lineFitter(x, y, (limits[i][0], limits[i][1]), verbose=True) 
+        x_adc[i], x_adc_err[i], sigma[i], sigma_err[i] =  result.params['center'].value, result.params['center'].stderr, \
+                                                    result.params['sigma'].value, result.params['sigma'].stderr
+           
+        if visualize:                                         
+            plt.plot(x_fine, fitting_curve, lw=2, color='red')
+                                  
+
+        if sigma_err[i] is None:
+            sigma_err[i]=0.
+    FWHM = 2.355*sigma
+    FWHM_err = 2.355*sigma_err
+    
+
+    # Sanification  
+    x_adc = [item if item else 0. for item in x_adc]
+    x_adc_err = [item if item else 0. for item in x_adc_err]
+    x_adc = [0 if np.isnan(item) else item for item in x_adc]
+    x_adc_err = [0 if np.isnan(item) else item for item in x_adc_err]
+    
+    fit_results = np.column_stack((x_adc, x_adc_err, FWHM, FWHM_err))
+    return fit_results
+
+
+def calibrate(ASIC,v,line_data, fit_results, mode='stretcher', verbose=True):
+    """
+    This function establish gain and offset values for each channel, with respective errors from the peaks fitted.
+    
+    Input:
+          line_data : array containing the energy line and the color associated
+          fit_results : array with the fit of the peaks
+    Output:
+          chi, gain, gain_err, offset, offset_err : floats
+        
+    """
+    if mode.lower() == 'stretcher':
+        colorlin='red'
+        colormark='tomato'
+        fmtmark='o'
+    elif mode.lower() == 'shaper':
+        colorlin='blue'
+        colormark='cornflowerblue'
+        fmtmark='s'
+        
+    adc = fit_results[:,0]
+    adc_err = fit_results[:,1]
+    energy_line = []
+    for key in sorted(line_data.keys()):
+        energy_line.append(line_data[key][0])
+    
+    energy_line.sort()
+
+    lmod = LinearModel()
+    pars = lmod.guess(adc, x=energy_line)
+    try:
+        resultlin = lmod.fit(adc, pars, x=energy_line, weights=adc_err)
+        figcal=plt.figure(figsize=(16,6))
+        plt.title('Linear Model & Residuals for CH '+str(v)+' ASIC '+ASIC)
+        resultlin.plot()
+        plt.savefig('./Quad'+ASIC+'/'+ASIC+'_FITLIN_CH_'+str(v)+'.png')
+        plt.close(figcal)
+    except TypeError:
+        print('xadc',x_adc)
+        print('xline',x_line)
+        print('xadcerr',x_adc_err)
+    #plt.figure()
+    #plt.errorbar(energy_line,adc, yerr=adc_err, fmt='ro',marker='s',label='data',color='red')
+    #plt.plot(energy_line,resultlin.best_fit,label='best fit',color='blue')
+    #plt.show()
+
+    chi= resultlin.redchi
+    gain = resultlin.params['slope'].value
+    offset = resultlin.params['intercept'].value
+    gain_err = resultlin.params['slope'].stderr
+    offset_err =resultlin.params['intercept'].stderr
+    
+    return chi, gain, gain_err, offset, offset_err, energy_line
