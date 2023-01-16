47c2efd0bd3a22c4b8fee5aaff42e40c0b08adfa
peppedilillo <peppedilillo@gmail.com>
2023-01-17 00:43:23 +0100

    removed linearity plots. housekeeping

--- source/eventlist.py
+++ source/eventlist.py
@@ -48,10 +48,10 @@ def _convert_gamma_events(data, scint_calibrations, couples):
     channel = out["CHN"] + qm
     scint_ucid = channel.map(companion_to_channel).fillna(channel).astype(int)
     ucid_calibs = _as_ucid_dataframe(scint_calibrations)
-    if scint_ucid.isin(ucid_calibs.index).all(bool_only=True):
+    if np.all(scint_ucid.isin(ucid_calibs.index).values):
         electrons = out["ELECTRONS"]
         light_outs = ucid_calibs.loc[scint_ucid]["light_out"].values
-    elif scint_ucid.isin(ucid_calibs.index).any(bool_only=True):
+    elif np.any(scint_ucid.isin(ucid_calibs.index).values):
         mask = scint_ucid.isin(ucid_calibs.index)
         electrons = out[mask]["ELECTRONS"]
         light_outs = ucid_calibs.loc[scint_ucid[mask]]["light_out"].values
